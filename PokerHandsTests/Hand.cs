5b8b395a58aab43adb4adacaa478da28cc61c6c8
Jonas Folleso <jonas@follesoe.no>
2010-05-18 23:06:56 +0200

    Initial commit.

---
+++ PokerHandsTests/Hand.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Linq;
+
+namespace PokerHandsTests
+{
+    public class Card
+    {
+        public char Value { get; private set; }
+        public char Suit { get; private set; }
+
+        public Card(string card)
+        {
+            Value = card[0];
+            Suit = card[1];
+        }
+
+        public override string ToString()
+        {
+            return Value + Suit.ToString();
+        }
+
+        public int NumericValue()
+        {
+            if (Value == 'J') return 11;
+            if (Value == 'Q') return 12;
+            if (Value == 'K') return 13;
+            if (Value == 'A') return 14;
+            return Convert.ToInt32(Value.ToString());
+        }
+
+        public bool IsSubsequentTo(Card card)
+        {
+            return NumericValue() - card.NumericValue() == 1;
+        }
+    }
+
+    public class Hand
+    {
+        private const string FlushFormat = "Flush ({0})";
+        private const string StraightFormat = "Straight ({0}) high";
+        private const string FourOfAKindFormat = "Four of a kind ({0})";
+        private const string ThreeOfAKindFormat = "Three of a kind ({0})";
+        private const string TwoPairFormat = "Two pair ({0}, {1})";
+        private const string OnePairFormat = "Pair ({0})";
+        private const string HighCardFormat = "High Card ({0})";
+
+        private readonly Card[] _cards;
+
+        public Hand(string cards)
+        {
+            _cards = (from card in cards.Split(' ')
+                      select new Card(card))
+                      .OrderBy(c => c.NumericValue())
+                      .ToArray();
+        }
+
+
+        internal string GetScore()
+        {
+            if (HasFlush()) return string.Format(FlushFormat, _cards[4].Suit);
+            if (HasStraight()) return string.Format(StraightFormat, _cards[4].Value);
+            if (HasFourOfAKind()) return string.Format(FourOfAKindFormat, _cards[0].Value);
+            if (HasThreeOfAKind()) return string.Format(ThreeOfAKindFormat, _cards[0].Value);
+            if (HasTwoPairs()) return string.Format(TwoPairFormat, _cards[0].Value, _cards[2].Value);
+            if (HasOnePair()) return string.Format(OnePairFormat, _cards[0].Value);
+            
+            return string.Format(HighCardFormat, _cards[4]);
+        }
+
+        private bool HasFlush()
+        {
+            for (int cardIndex = 1; cardIndex < 5; cardIndex++)
+            {
+                if (_cards[cardIndex].Suit != _cards[cardIndex - 1].Suit)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        private bool HasStraight()
+        {
+            for (int cardIndex = 1; cardIndex < 5; cardIndex++)
+            {
+                if (!_cards[cardIndex].IsSubsequentTo(_cards[cardIndex - 1]))
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        private bool HasOnePair()
+        {
+            return _cards[0].Value == _cards[1].Value;
+        }
+
+        private bool HasTwoPairs()
+        {
+            return _cards[0].Value == _cards[1].Value &&
+                   _cards[2].Value == _cards[3].Value;
+        }
+
+        private bool HasThreeOfAKind()
+        {
+            return _cards[0].Value == _cards[1].Value &&
+                   _cards[1].Value == _cards[2].Value;
+        }
+
+        private bool HasFourOfAKind()
+        {
+            return _cards[0].Value == _cards[1].Value &&
+                   _cards[1].Value == _cards[2].Value &&
+                   _cards[2].Value == _cards[3].Value;
+        }
+    }
+}
---
+++ PokerHandsTests/PokerHandTests.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PokerHandsTests
+{
+    [TestClass]
+    public class PokerHandTests
+    {  
+        [TestMethod]
+        public void Hand_has_high_card_KD()
+        {
+            assertThatHandHasHighCard("2H 3D 5S 9C KD", "KD");
+            assertThatHandHasHighCard("2H 3D QD 9C 5C", "QD");
+            assertThatHandHasHighCard("2H 3D JH 9C 4H", "JH");
+        }
+
+        private void assertThatHandHasHighCard(string hand, string card)
+        {
+            var hand1 = new Hand(hand);
+            string score = hand1.GetScore();
+
+            Assert.AreEqual(string.Format("High Card ({0})", card), score);
+        }
+        
+        [TestMethod]
+        public void Hand_has_one_Pair()
+        {
+            var hand1 = new Hand("2H 3D JH 9C 2D");
+            string score = hand1.GetScore();
+
+            Assert.AreEqual("Pair (2)", score);
+        }
+
+        [TestMethod]
+        public void Hand_has_two_pairs()
+        {
+            var hand1 = new Hand("2H 3D JH 3C 2D");
+            string score = hand1.GetScore();
+
+            Assert.AreEqual("Two pair (2, 3)", score);
+        }
+
+        [TestMethod]
+        public void Hand_has_three_of_a_kind()
+        {
+            var hand1 = new Hand("2H 3D JH 2S 2D");
+            string score = hand1.GetScore();
+
+            Assert.AreEqual("Three of a kind (2)", score);
+        }
+
+        [TestMethod]
+        public void Hand_has_four_of_a_kind()
+        {
+            var hand1 = new Hand("2H 3D 2C 2S 2D");
+            string score = hand1.GetScore();
+
+            Assert.AreEqual("Four of a kind (2)", score);
+        }
+
+        [TestMethod]
+        public void Hand_has_straight()
+        {
+            var hand1 = new Hand("3H 5C 4S 6C 7S");
+            string score = hand1.GetScore();
+
+            Assert.AreEqual("Straight (7) high", score);
+        }
+
+        [TestMethod]
+        public void Hand_has_flush()
+        {
+            var hand1 = new Hand("3H 5H 3H 4H 9H");
+            string score = hand1.GetScore();
+
+            Assert.AreEqual("Flush (H)", score);
+        }
+    }
+}
